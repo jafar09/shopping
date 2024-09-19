c0ebe4bb752814905f29b40ed214d11434340379
jafar09 <jafarshomurodov22@gmail.com>
2024-09-19 15:04:33 +0500

    html o'zgardi

--- my_shopping/my_shopping/settings.py
+++ my_shopping/my_shopping/settings.py
@@ -118,7 +118,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS =[
-os.path.join(BASE_DIR, 'static')]
+os.path.join(BASE_DIR, 'assets')]
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'assets/img')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
